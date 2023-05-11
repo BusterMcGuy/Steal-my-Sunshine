a1ff20887618f506efa4136eced76497b52ab5f7
BusterMcGuy <emsery@gmail.com>
2023-05-11 11:02:30 -0400

    Made some balls, one with mouse, collison idk

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -4,6 +4,11 @@
 
 int main(void)
 {
+
+
+    const int screenWidth = 1000;
+    const int screenHeight = 1000;
+
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
     SetTargetFPS(60);
 
@@ -12,6 +17,37 @@ int main(void)
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("Hello World!", 16, 9, 20, RED);
+        //int centreX = 100;
+        //int centreY = 100;
+        //float radius = 30;
+
+        Vector2 GetMousePosition(void);
+
+
+        Vector2 ballPosition2 = { -100.0f, -100.0f };
+
+        Vector2 ballposition1 = { 100.0f, 100.0f };
+
+        Color ballColor1 = DARKBROWN;
+
+        Color ballColor2 = DARKGREEN;
+
+        ballPosition2 = GetMousePosition();
+
+        DrawCircleV(ballPosition2, 50, ballColor2);
+
+        DrawCircleV(ballposition1, 50, ballColor1);
+
+        DrawCircle(screenWidth / 5, 120, 35, DARKBLUE);
+
+        bool IsKeyPressed(int s);
+
+        bool CheckCollisionCircles(Vector2 center1, float radius1, Vector2 center2, float radius2);
+      
+        CheckCollisionCircles(ballposition1, 50, ballPosition2, 50);
+        if(CheckCollisionCircles = true)
+       
+
         EndDrawing();
     }
 
